feea575ffd70d065e5dd942b71aef9db5b905425
Achhe Code <achhecode@gmail.com>
2025-11-04 22:21:40 +0530

    Added java file similar to python file

---
+++ Java/Java/src/Main2.java
@@ -0,0 +1,21 @@
+public class Main2 {
+    static void main() {
+        int myAge = 150;
+        String channelName = "Achhe Code";
+        boolean isChannelActive = true;
+
+        System.out.println(myAge);
+        System.out.println(channelName);
+        System.out.println(isChannelActive);
+
+        System.out.println(myAge-50);
+
+
+        System.out.println("My channel name is " + channelName);
+
+        double PI = 3.1415926;
+
+        System.out.println("PI value is "+PI);
+
+    }
+}
