786b6c7bad4b0a044161d6dc307a59bd6653668e
Achhe Code <achhecode@gmail.com>
2025-11-04 22:21:25 +0530

    Updated main file

---
+++ Python/main.py
@@ -0,0 +1,15 @@
+myAge = 150
+channelName = "Achhe Code"
+isChannelActive = True
+
+print(myAge)
+print(channelName)
+print(isChannelActive)
+
+print(myAge-50)
+
+
+print("My channel name is " + channelName)
+
+PI = 3.1415926
+
